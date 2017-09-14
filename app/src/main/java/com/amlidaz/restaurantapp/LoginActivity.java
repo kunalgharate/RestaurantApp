9f0b273eb0ca945197742e89e2c7f66492af1578
admin <sumitgharate@gmail.com>
2017-09-14 14:16:49 +0530

    Forgot Password Updated

--- app/src/main/java/com/amlidaz/restaurantapp/LoginActivity.java
+++ app/src/main/java/com/amlidaz/restaurantapp/LoginActivity.java
@@ -3,11 +3,12 @@ package com.amlidaz.restaurantapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,13 +26,17 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    TextView linkSignup;
+    TextView linkSignup, reset_pass;
     EditText txtEmail, txtPassword;
     Button btnLogin;
+    String email, password;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    String email,password;
     private ProgressBar progressBar;
+
+    public final static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         linkSignup = (TextView) findViewById(R.id.link_signup);
+        reset_pass = (TextView) findViewById(R.id.txtforgot_pass);
         txtEmail = (EditText) findViewById(R.id.input_email);
         txtPassword = (EditText) findViewById(R.id.input_password);
         btnLogin = (Button) findViewById(R.id.btn_login);
@@ -66,6 +72,13 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        reset_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
             }
         });
 
@@ -127,7 +140,49 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public final static boolean isValidEmail(CharSequence target){
-        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    private void resetPassword() {
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.activity_reset_password, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editEmail = (EditText) dialogView.findViewById(R.id.email);
+        final Button btnReset = (Button) dialogView.findViewById(R.id.btn_reset_password);
+        final ProgressBar progressBar1 = (ProgressBar) dialogView.findViewById(R.id.progressBar);
+
+
+        //dialogBuilder.setTitle("Send Photos");
+        final AlertDialog dialog = dialogBuilder.create();
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email = editEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar1.setVisibility(View.VISIBLE);
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
+
+                                progressBar1.setVisibility(View.GONE);
+                                dialog.dismiss();
+                            }
+                        });
+
+            }
+        });
+        dialog.show();
     }
+
+
 }
