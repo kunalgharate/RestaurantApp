74735586b74854d39572cb5c0512a3fea728c9db
admin <sumitgharate@gmail.com>
2017-10-04 14:55:08 +0530

    DashBoard Added FlashScreen added Menu Added Waiter Linked or Product type list created

--- app/src/main/java/com/amlidaz/restaurantapp/adminUploadNewItem.java
+++ app/src/main/java/com/amlidaz/restaurantapp/adminUploadNewItem.java
@@ -8,11 +8,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.amlidaz.restaurantapp.model.ImageUpload;
@@ -28,11 +33,12 @@ import com.google.firebase.storage.UploadTask;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class adminUploadNewItem extends AppCompatActivity {
+public class adminUploadNewItem extends AppCompatActivity implements View.OnClickListener {
 
     public static final String FB_Storage_Path = "image/";
     public static final String FB_Database_Path = "image";
     public static final int Request_Code = 1234;
+    Button btnUploadImage, btnAddProductType, btnUploadItem;
     private StorageReference storageReference;
     private DatabaseReference databaseReference;
     private ImageView imageView;
@@ -51,14 +57,22 @@ public class adminUploadNewItem extends AppCompatActivity {
         editTextItemName = (EditText) findViewById(R.id.editTextItemName);
         editTextItemDesc = (EditText) findViewById(R.id.editTextItemDesc);
         editTextItemPrice = (EditText) findViewById(R.id.editTextItemPrice);
+        btnAddProductType = (Button) findViewById(R.id.btn_addnewproduct);
+        btnUploadImage = (Button) findViewById(R.id.btn_uploadimage);
+        btnUploadItem = (Button) findViewById(R.id.btn_submitdata);
+
+        btnAddProductType.setOnClickListener(this);
+        btnUploadImage.setOnClickListener(this);
+        btnUploadItem.setOnClickListener(this);
+
     }
 
-    public void btnBrowse_Click(View v) {
+   /* public void btnBrowse_Click(View v) {
         Intent intent = new Intent();
-        intent.setType("image/*");
+        intent.setType("image*//*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select image"), Request_Code);
-    }
+    }*/
 
 
     @Override
@@ -85,7 +99,23 @@ public class adminUploadNewItem extends AppCompatActivity {
     }
 
     @SuppressWarnings("VisibleForTests")
-    public void btnUpload_Click(View v) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_uploadimage:
+                //Browse Image
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select image"), Request_Code);
+                break;
+            case R.id.btn_addnewproduct:
+
+                addProductType();
+                break;
+            case R.id.btn_submitdata:
+                //Submit Data Ti firebase
+
         if (imguri != null) {
             final ProgressDialog dialog = new ProgressDialog(this);
             dialog.setTitle("Uploading image");
@@ -142,12 +172,62 @@ public class adminUploadNewItem extends AppCompatActivity {
         } else {
             Toast.makeText(getApplicationContext(), "Please select image", Toast.LENGTH_SHORT).show();
         }
+                break;
+        }
 
 
     }
 
-    public void btnShowListImage_Click(View v) {
-        Intent i = new Intent(adminUploadNewItem.this, AdminDashboard.class);
-        startActivity(i);
+    private void addProductType() {
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.activity_add_product_type, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText neweditproductype = (EditText) dialogView.findViewById(R.id.edit_product_type);
+        final Button btnAddProductType = (Button) dialogView.findViewById(R.id.btn_add_product_type);
+        final Button btnCancelProductType = (Button) dialogView.findViewById(R.id.btn_cancel_product_type);
+        final ProgressBar progressBar1 = (ProgressBar) dialogView.findViewById(R.id.progressBar);
+
+
+        //dialogBuilder.setTitle("Send Photos");
+        final AlertDialog dialog = dialogBuilder.create();
+
+        btnCancelProductType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        btnAddProductType.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String editproductype = neweditproductype.getText().toString().trim();
+
+                if (TextUtils.isEmpty(editproductype)) {
+                    Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+               /* progressBar1.setVisibility(View.VISIBLE);
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
+
+                                progressBar1.setVisibility(View.GONE);
+                                dialog.dismiss();
+                            }
+                        });*/
+
+            }
+        });
+        dialog.show();
     }
+
+
 }
